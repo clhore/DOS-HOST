5ef334845569c296ae2fbe2d965c20b3bb8bc5b8
Adrián <79727938+clhore@users.noreply.github.com>
2021-11-02 19:32:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,5 +52,5 @@ if [ "$(id -u)" == "0" ]; then
 	dependencies
 	installPythonLibrary
 else
-	echo -e 'chmod +x ./install.sh && sudo ./install.sh'
+	echo -e 'sudo chmod +x ./install.sh && sudo ./install.sh'
 fi
